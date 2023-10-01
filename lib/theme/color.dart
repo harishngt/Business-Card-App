d2e2fdb53260a2a8d5263a148bac5434e0ce1197
harishngt <97893600+harishngt@users.noreply.github.com>
2023-10-01 21:44:21 +0530

    Create color.dart

---
+++ lib/theme/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primary = Color(0xFFd2e8d4);
+  static const logobg = Color(0xFF073042);
+  static const fontclr = Color(0xFF3ddc84);
+}

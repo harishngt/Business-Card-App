0d5465c5b095138f5c99e8164e172061d8b9e201
harishngt <97893600+harishngt@users.noreply.github.com>
2023-10-12 17:49:36 +0530

    Updated Assets and Theme Data

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const primary = Color(0xFFd2e8d4);
   static const logobg = Color(0xFF073042);
-  static const fontclr = Color(0xFF3ddc84);
+  static const fontclr = Color(0xFF00703c);
 }
